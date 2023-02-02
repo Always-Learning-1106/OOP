9a7abe95e621d4c5211652cc9d314eb2d24138c0
Glenn <1106glenn@gmail.com>
2023-02-01 20:24:30 -0500

    working on frecodecamp problem

--- script.js
+++ script.js
@@ -128,3 +128,61 @@
 //1. classes are not hoisted
 //2. Classes are first class citizens
 //3. the body of class is executed in strict mode
+let arr = [4, 5, 9];
+let multiply = function (arr, n) {
+  if (n <= 0) {
+    return 1;
+  } else {
+    return multiply(arr, n - 1) * arr[n - 1];
+  }
+};
+console.log(multiply(arr, 3));
+
+let sum = function (arr, n) {
+  if (n <= 0) {
+    return 0;
+  } else {
+    return sum(arr, n - 1) + arr[n - 1];
+  }
+};
+console.log(sum(arr, 3));
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+  return contacts["firstName"] === name && contacts[prop]
+    ? contacts[prop]
+    : !contacts["firstName"] && !contacts["lastName"]
+    ? "No such contact"
+    : !contacts[prop]
+    ? "No such property"
+    : "No such property";
+  // Only change code above this line
+}
+
+lookUpProfile("Akira", "likes");
